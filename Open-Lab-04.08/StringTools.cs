7f4f8c1bf5bccfe23269c0baf7a758b313b901a5
Lukáš Gašpierik <Luk Gapierik@LAPTOP-GBRVOBEH>
2020-11-01 19:41:36 +0100

    HOTOVO!

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_04._08
 {
@@ -6,7 +7,15 @@ namespace Open_Lab_04._08
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> l = new List<string>();
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == 4)
+                    l.Add(strings[i]);
+
+
+            }
+            return l.ToArray();
         }
     }
 }
